01aa564130daff0d1df710317f48fe9975a487e0
shenko <shenko.org@gmail.com>
2020-05-12 16:16:54 -0400

    Bump version: 0.1.19 → 0.1.20

--- _version.py
+++ _version.py
@@ -1,1 +1,1 @@
-__version__ = "version='0.1.19'"
+__version__ = "version='0.1.20'"
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = [
 
 setup(
     name='shenko',
-    version='0.1.19',
+    version='0.1.20',
     description="visit us at www.shenko.org",
     long_description=readme + '\n\n' + history,
     author="Danny Dowshenko",
