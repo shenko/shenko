7d0d18e56df4d32086276639e5e8f3907bd61298
shenko <shenko.org@gmail.com>
2020-06-19 17:52:58 -0400

    removed panda runtime from Central.py'

--- shenko/S05_CENTRAL/CENTRAL.py
+++ shenko/S05_CENTRAL/CENTRAL.py
@@ -11,7 +11,7 @@ MAIN MENU that threads
 """
 import sys
 from direct.showbase.ShowBase import ShowBase
- 
+
 class MyApp(ShowBase):
  
     def __init__(self):
@@ -19,31 +19,32 @@ class MyApp(ShowBase):
  
         # Load the environment model.
         self.scene = self.loader.loadModel("models/environment")
+        #self.scene = self.loader.loadModel("/media/shenko/shenko_HQ/0-TOC/shenko_website/website/public_html/public/library/civil/derp.gltf")
         # Reparent the model to render.
         self.scene.reparentTo(self.render)
         # Apply scale and position transforms on the model.
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-8, 42, 0)
+        self.scene.setScale(1, 1, 1)    # 1:1 scale
+        self.scene.setPos(0, 75, 0)     # X, Z, Y
+
+        # Turns on/off wireframe mode
+        self.scene.setRenderModeWireframe()
 
         # My first key binding
         self.accept('escape', self.quit)
-        self.accept('arrow_down-repeat', self.moveCam)
+        self.accept('arrow_down', self.moveCamDown)
 
     def quit(self):
     	print("quitting shenko")
     	sys.exit()
 
-    def moveCam(self):
-        global camZ
-        print("moving camera")
-        camZ -= 5
-        base.camera.setPos(camX, camY, camZ)
+    def moveCamDown(self):
+        print("Camera Function Not enabled")
 
 def central():
     print("central: OK")
     print("LAUNCHING APP SEE /CENTRAL.PY")
-    app = MyApp()
-    app.run()
+    #app = MyApp()
+    #app.run()
 
 # For making modules"
 if __name__ == '__main__':
