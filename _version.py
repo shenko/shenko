814b873dbec8cf7d00471df0f134a10765bae4f0
shenko <shenko.org@gmail.com>
2021-09-04 22:07:33 -0400

    Bump version: 0.1.56 → 0.1.57

--- _version.py
+++ _version.py
@@ -1,1 +1,1 @@
-__version__ = "version='0.1.56'"
+__version__ = "version='0.1.57'"
--- shenko/__init__.py
+++ shenko/__init__.py
@@ -4,5 +4,5 @@
 
 __author__ = """Danny Dowshenko"""
 __email__ = 'dowshenko225@gmail.com'
-__version__ = '0.1.56'
+__version__ = '0.1.57'
 import shenko
