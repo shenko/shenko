9fc74ce786b54f670b74a01cb04d6e0a31173c6e
shenko <shenko.org@gmail.com>
2021-09-04 21:15:41 -0400

    Bump version: 0.1.52 → 0.1.53

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = [
 
 setup(
     name='shenko',
-    version='0.1.52',
+    version='0.1.53',
     description="visit us at www.shenko.org",
     long_description=readme + '\n\n' + history,
     author="Danny Dowshenko",
