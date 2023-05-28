1040face803abc93c52d74e0eaf402e8bebc4794
Danny Dowshenko <danny.dowshenko@shenko.org>
2023-05-28 19:19:43 -0400

    Bump version: 0.1.68 → 0.1.69

--- _version.py
+++ _version.py
@@ -1,1 +1,1 @@
-__version__ = "version='0.1.68'"
+__version__ = "version='0.1.69'"
--- shenko/__init__.py
+++ shenko/__init__.py
@@ -4,5 +4,5 @@
 
 __author__ = """Danny Dowshenko"""
 __email__ = 'dowshenko225@gmail.com'
-__version__ = '0.1.68'
+__version__ = '0.1.69'
 import shenko
