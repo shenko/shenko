3bd11b0cdc99f91677209f6ac45520ffa95ab298
Danny Dowshenko <danny.dowshenko@shenko.org>
2023-05-28 18:06:32 -0400

    Bump version: 0.1.67 → 0.1.68

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = [
 
 setup(
     name='shenko',
-    version='0.1.67',
+    version='0.1.68',
     description="visit us at www.shenko.org",
     long_description=readme + '\n\n' + history,
     author="Danny Dowshenko",
