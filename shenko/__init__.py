65ecb1319ea24fbbd5aa755641121d139f793d69
Danny Dowshenko <danny.dowshenko@shenko.org>
2024-05-19 14:22:29 -0400

    Bump version: 0.1.83 → 0.1.84

--- shenko/__init__.py
+++ shenko/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Danny Dowshenko"""
 __email__ = 'dowshenko225@gmail.com'
-__version__ = '0.1.83'
+__version__ = '0.1.84'
 from menu import AppState
 from menu import quit
 from menu import toggle_fullscreen
